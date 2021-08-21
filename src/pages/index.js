93a093fee181ae89efa5d04e21f2b2e9e4a7f689
Abdul Kalam Azad <azadak.ker@gmail.com>
2021-08-21 21:14:05 +0530

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Home from './HomePage'
+import About from './AboutPage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import Error from './ErrorPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import  PrivateRoute from './PrivateRoute'
+import AuthWrapper from './AuthWrapper'
+import { StripeCheckout } from '../components'
+
+export { Home,About,Cart,Checkout,Error,Products,SingleProduct,PrivateRoute,AuthWrapper, StripeCheckout }
